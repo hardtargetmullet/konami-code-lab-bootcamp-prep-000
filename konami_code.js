b2795e6fd97b166f2a2b3206a8b0bdf5bfd96ee9
Abraham Nieto <elmeromero.an@gmail.com>
2018-01-01 02:12:07 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,11 +3,20 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   // Write your JavaScript code inside the init() function
   const b = document.querySelector('body')
-  console.log(b)
+  //console.log(b)
   
-  b.addEventListener('keydown', function(e) { {
-        console.log(e.detail || e.which)
-      }
-  });
+  //b.addEventListener('keydown', function(e) { {
+  //      console.log(e.detail || e.which)
+  //    }
+  //});
+  let index = 0;
+  
+  b.addEventListener('keydown', function onKeyDownHandler(e) {
+    const key = parseInt(e.detail || e.which);
+    
+    if (key === alphabet[index]) {
+      alert("Hurray!")
+    }
+  })
  
 }
