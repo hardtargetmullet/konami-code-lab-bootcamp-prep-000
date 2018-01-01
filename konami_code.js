c654277b6b9e6343bac0a1b959ba48bcbf26d443
Abraham Nieto <elmeromero.an@gmail.com>
2018-01-01 02:31:17 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,5 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-const code_str = ["ArrowUp"]
+const code_str = ["ArrowUp", "ArrowUp"]
 function init() {
   // Write your JavaScript code inside the init() function
   const b = document
